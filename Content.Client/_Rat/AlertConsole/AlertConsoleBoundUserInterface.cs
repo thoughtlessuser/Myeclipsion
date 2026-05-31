40e0f2c2b0162abf1989069cd73ff7497a5f9bd1
Mirage_exe <mirageeexe@gmail.com>
2026-05-31 16:58:24 +0300

    фикс, теперь не срет
    Больше не реагирует на все гриды вокруг, только на шаттлы с опознаванием. Изначально выключена

--- Content.Client/_Rat/AlertConsole/AlertConsoleBoundUserInterface.cs
+++ Content.Client/_Rat/AlertConsole/AlertConsoleBoundUserInterface.cs
@@ -23,7 +23,6 @@ public sealed class AlertConsoleBoundUserInterface : BoundUserInterface
             SendMessage(new AlertConsoleSaveSettingsMessage(
                 settings.Enabled,
                 settings.DetectionRadius,
-                settings.FactionChannel,
                 settings.StationAlertMessage,
                 settings.BroadcastToShuttle,
                 settings.ShuttleAlertMessage,
